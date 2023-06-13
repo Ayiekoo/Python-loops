675df5eba40c0924546f3a2b1bd6fdd71cbe2569
Ayiekoo <131998667+Ayiekoo@users.noreply.github.com>
2023-06-14 02:29:14 +0300

    Add files via upload

---
+++ break and continue.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+## break
+
+##### ends loop prematurely
+
+while(True):
+    break # no infinite loop
+print("Today's Wednesday")
+
+
+# In[ ]:
+
+
+### continue
+
+#### finishes current loop iteration
+
+while(True):
+    continue
+    print("28")
+### dead code
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
